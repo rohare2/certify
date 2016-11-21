9d85dfd040926bea0e7c3af1f03fb47cdc9eb763
Rich O'Hare <ohare2@llnl.gov>
2016-11-21 14:03:49 -0800

    removed the whole db section

--- db/inventory_AINS.sql
+++
@@ -1,6 +0,0 @@
-USE `disks`;
-DELIMITER $$
-CREATE TRIGGER `inventory_AINS` AFTER INSERT ON `inventory` FOR EACH ROW
-BEGIN
-	INSERT INTO history (location, vendor, product, model, serialNo, state, chg_by, chg_date, description) VALUES (new.location, new.vendor, new.product, new.model, new.serialNo, new.state, new.chg_by, new.chg_date, new.description);
-END;
--- db/inventory_AUPD.sql
+++
@@ -1,9 +0,0 @@
-USE `disks`;
-DELIMITER $$
-CREATE TRIGGER `inventory_AUPD` AFTER UPDATE ON `inventory` FOR EACH ROW
-BEGIN
-	IF old.state != new.state THEN
-		INSERT INTO history (location,vendor,product,model,serialNo,state,chg_by,chg_date,description)
-		VALUES (new.location,new.vendor,new.product,new.model,new.serialNo,new.state,new.chg_by,new.chg_date,new.description);
-	END IF;
-END;
