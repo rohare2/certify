db0b6e8952965ba9f0f5d0cf3d1d30579725e369
Rich O'Hare <ohare2@llnl.gov>
2018-03-09 08:40:47 -0800

    put braces around dir variable

--- clamscan.sh
+++ clamscan.sh
@@ -7,14 +7,14 @@ dirs="/bin /boot /etc /home /lib /lib64 /opt /root /sbin /usr /var"
 
 dscan() {
 	dir=$1
-	if [ -d $dir ]; then
+	if [ -d ${dir} ]; then
 		logger -t clamav "${dir}"
-		ret=`/bin/find $dir -type f ! -fstype nfs -mtime -2 -print0 | xargs -0 -r clamscan -i | grep '^Infected' 2>&1`
+		ret=`/bin/find ${dir} -type f ! -fstype nfs -mtime -2 -print0 | xargs -0 -r clamscan -i | grep '^Infected' 2>&1`
 		logger -t clamav "${ret}"
 	fi
 }
 
-for LINE in $dirs
+for LINE in ${dirs}
 do
 	dscan ${LINE}
 done
