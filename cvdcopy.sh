5eb7251673c2143fc107d0555e9c671c91e18c25
Rich O'Hare <richard.ohare@me.com>
2018-03-21 20:59:58 -0700

    modifying clamav routines

--- cvdcopy.sh
+++ cvdcopy.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-# cvdcopy
-## Copy the clam .cvd files on the web server
-cp /var/lib/clamav/*.cvd /var/www/html/software/VendorSoftware/clam/
+# cvdcopy.sh
+## Copy the clam .cvd files to the web server
+
+# This script is only applicable for providing a custom source.
+# It copies the virus definition files from the standard ClamAV
+# location to a local web server. ClamAV clients than use wget
+# aquire the files.
+#
+enableClamav=0
+clamavServer=0
+
+if [ $enabled == "1" ]; then
+	if [ $clamavServer == "1" ]; then
+		cp /var/lib/clamav/*.cvd /var/www/html/software/VendorSoftware/clam/
+	fi
+fi
